9dc92e0e11b0ca1c422c552c25eac6899a945750
Leonardo Chermaut <101607060+LeonardoChermaut@users.noreply.github.com>
2023-06-19 11:35:05 -0300

    chore: add typeorm config

---
+++ src/config/typeorm.config.ts
@@ -0,0 +1,15 @@
+import { ConfigService } from '@nestjs/config';
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+
+export const configuration = (
+  configService: ConfigService,
+): TypeOrmModuleOptions => ({
+  type: 'mariadb',
+  host: configService.get(process.env.DB_HOST, 'localhost'),
+  port: Number(configService.get(process.env.DB_PORT, '3307')),
+  username: configService.get(process.env.DB_USERNAME, 'root'),
+  password: configService.get(process.env.DB_PASSWORD, 'root'),
+  database: configService.get(process.env.DB_DATABASE, 'todoapp'),
+  synchronize: true,
+  entities: ['dist/**/*.entity.{ts,js}'],
+});
