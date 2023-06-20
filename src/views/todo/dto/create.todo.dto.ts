035720b14653051713371a3184ab660bf6343271
Leonardo Chermaut <101607060+LeonardoChermaut@users.noreply.github.com>
2023-06-20 16:57:43 -0300

    refactor: adjust type properties dto

--- src/views/todo/dto/create.todo.dto.ts
+++ src/views/todo/dto/create.todo.dto.ts
@@ -2,12 +2,12 @@ import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
 import { IsIn, IsNotEmpty } from 'class-validator';
 
 export class CreateTodoDto {
-  @IsNotEmpty()
-  @ApiProperty()
+  @IsNotEmpty({ message: 'Task is required' })
+  @ApiProperty({ type: String, description: 'The task to do' })
   task: string;
 
   @IsNotEmpty()
   @IsIn([true, false])
-  @ApiPropertyOptional()
+  @ApiPropertyOptional({ type: Boolean, default: false })
   isDone: boolean;
 }
