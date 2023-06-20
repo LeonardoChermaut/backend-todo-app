7c0b3a4547aee9b7400c072020dee05f5083ae82
Leonardo Chermaut <101607060+LeonardoChermaut@users.noreply.github.com>
2023-06-20 13:50:51 -0300

    refactor: complete test controller

--- src/views/todo/_mock/mock.ts
+++ src/views/todo/_mock/mock.ts
@@ -1,7 +1,7 @@
 import { TodoEntity } from '../../../entity/todo.entity';
 import { CreateTodoDto, UpdateTodoDto } from '../dto';
 
-export const listTodoMock: TodoEntity[] = [
+export const listTodoEntityMock: TodoEntity[] = [
   new TodoEntity({ id: 1, task: 'Todo 1', isDone: 0 }),
   new TodoEntity({ id: 2, task: 'Todo 2', isDone: 0 }),
   new TodoEntity({ id: 3, task: 'Todo 3', isDone: 0 }),
@@ -9,10 +9,10 @@ export const listTodoMock: TodoEntity[] = [
   new TodoEntity({ task: 'Todo 100', isDone: 1 }),
 ];
 
-export const todoMock: TodoEntity = { ...listTodoMock[3] };
+export const todoEntityMock: TodoEntity = { ...listTodoEntityMock[3] };
 
-export const createTodoMock: CreateTodoDto = { ...todoMock };
+export const createTodoMock: CreateTodoDto = { ...todoEntityMock };
 
-export const updateTodoMock: UpdateTodoDto = { ...listTodoMock[4] };
+export const updateTodoMock: UpdateTodoDto = { ...todoEntityMock };
 
 export const bodyUpdateTodoMock: UpdateTodoDto = { ...updateTodoMock };
--- src/views/todo/todo.controller.spec.ts
+++ src/views/todo/todo.controller.spec.ts
@@ -4,14 +4,14 @@ import { TodoService } from './todo.service';
 import {
   bodyUpdateTodoMock,
   createTodoMock,
-  listTodoMock,
-  todoMock,
+  listTodoEntityMock,
+  todoEntityMock,
   updateTodoMock,
 } from './_mock';
 
 describe('TodoController', () => {
   let todoController: TodoController;
-  let service: TodoService;
+  let todoService: TodoService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -20,9 +20,9 @@ describe('TodoController', () => {
         {
           provide: TodoService,
           useValue: {
-            findAll: jest.fn().mockResolvedValue(listTodoMock),
-            create: jest.fn().mockResolvedValue(todoMock),
-            findOneOrFail: jest.fn().mockResolvedValue(listTodoMock[0]),
+            findAll: jest.fn().mockResolvedValue(listTodoEntityMock),
+            create: jest.fn().mockResolvedValue(todoEntityMock),
+            findOneOrFail: jest.fn().mockResolvedValue(listTodoEntityMock[0]),
             update: jest.fn().mockResolvedValue(updateTodoMock),
             deleteById: jest.fn().mockResolvedValue(undefined),
           },
@@ -31,48 +31,50 @@ describe('TodoController', () => {
     }).compile();
 
     todoController = module.get<TodoController>(TodoController);
-    service = module.get<TodoService>(TodoService);
+    todoService = module.get<TodoService>(TodoService);
   });
 
   it('should be defined', () => {
     expect(todoController).toBeDefined();
-    expect(service).toBeDefined();
+    expect(todoService).toBeDefined();
   });
 
   describe('should return a todo list entity successfully', () => {
     it('should be defined', async () => {
       const result = await todoController.index();
-      expect(result).toEqual(listTodoMock);
+      expect(result).toEqual(listTodoEntityMock);
     });
   });
 
   it('should throw an exception', async () => {
-    jest.spyOn(service, 'findAll').mockRejectedValueOnce(new Error());
+    jest.spyOn(todoService, 'findAll').mockRejectedValueOnce(new Error());
     expect(todoController.index()).rejects.toThrowError();
   });
 
   describe('should be return a new entity create', () => {
     it('should be defined', async () => {
       const result = await todoController.create(createTodoMock);
-      expect(result).toEqual(todoMock);
+      expect(result).toEqual(todoEntityMock);
     });
   });
 
   it('should throw an exception', async () => {
-    jest.spyOn(service, 'create').mockRejectedValueOnce(new Error());
+    jest.spyOn(todoService, 'create').mockRejectedValueOnce(new Error());
     expect(todoController.create(createTodoMock)).rejects.toThrowError();
   });
 
   describe('show', () => {
     it('should get a todo item successfully', async () => {
       const result = await todoController.show(1);
-      expect(result).toEqual(listTodoMock[0]);
-      expect(service.findOneOrFail).toBeCalledTimes(1);
-      expect(service.findOneOrFail).toBeCalledWith(1);
+      expect(result).toEqual(listTodoEntityMock[0]);
+      expect(todoService.findOneOrFail).toBeCalledTimes(1);
+      expect(todoService.findOneOrFail).toBeCalledWith(1);
     });
 
     it('should throw an exception', async () => {
-      jest.spyOn(service, 'findOneOrFail').mockRejectedValueOnce(new Error());
+      jest
+        .spyOn(todoService, 'findOneOrFail')
+        .mockRejectedValueOnce(new Error());
       expect(todoController.show(1)).rejects.toThrowError();
     });
   });
@@ -82,13 +84,13 @@ describe('TodoController', () => {
       const result = await todoController.update(1, bodyUpdateTodoMock);
 
       expect(result).toEqual(updateTodoMock);
-      expect(service.update).toBeCalledTimes(1);
-      expect(service.update).toBeCalledWith(1, bodyUpdateTodoMock);
+      expect(todoService.update).toBeCalledTimes(1);
+      expect(todoService.update).toBeCalledWith(1, bodyUpdateTodoMock);
     });
   });
 
   it('should throw an exception', async () => {
-    jest.spyOn(service, 'update').mockRejectedValueOnce(new Error());
+    jest.spyOn(todoService, 'update').mockRejectedValueOnce(new Error());
     expect(todoController.update(1, bodyUpdateTodoMock)).rejects.toThrowError();
   });
 
@@ -97,13 +99,13 @@ describe('TodoController', () => {
       const result = await todoController.destroy(1);
 
       expect(result).toBeUndefined();
-      expect(service.deleteById).toBeCalledTimes(1);
-      expect(service.deleteById).toBeCalledWith(1);
+      expect(todoService.deleteById).toBeCalledTimes(1);
+      expect(todoService.deleteById).toBeCalledWith(1);
     });
   });
 
   it('should throw an exception', async () => {
-    jest.spyOn(service, 'deleteById').mockRejectedValueOnce(new Error());
+    jest.spyOn(todoService, 'deleteById').mockRejectedValueOnce(new Error());
     expect(todoController.destroy(1)).rejects.toThrowError();
   });
 });
