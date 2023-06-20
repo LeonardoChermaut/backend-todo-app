a8caf420328f4c09316b63ae48c20311d5bd5bbd
Leonardo Chermaut <101607060+LeonardoChermaut@users.noreply.github.com>
2023-06-20 10:42:48 -0300

    feat: test to index findall mock

--- src/views/todo/todo.controller.spec.ts
+++ src/views/todo/todo.controller.spec.ts
@@ -1,18 +1,42 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { TodoController } from './todo.controller';
+import { TodoService } from './todo.service';
+import { todoListMock } from './_mock';
 
 describe('TodoController', () => {
-  let controller: TodoController;
+  let todoController: TodoController;
+  let service: TodoService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [TodoController],
+      providers: [
+        {
+          provide: TodoService,
+          useValue: {
+            findAll: jest.fn().mockResolvedValue(todoListMock),
+            create: jest.fn(),
+            findOneOrFail: jest.fn(),
+            update: jest.fn(),
+            deleteById: jest.fn(),
+          },
+        },
+      ],
     }).compile();
 
-    controller = module.get<TodoController>(TodoController);
+    todoController = module.get<TodoController>(TodoController);
+    service = module.get<TodoService>(TodoService);
   });
 
   it('should be defined', () => {
-    expect(controller).toBeDefined();
+    expect(todoController).toBeDefined();
+    expect(service).toBeDefined();
+  });
+
+  describe('should return a todo list entity successfully', () => {
+    it('should be defined', async () => {
+      const result = await todoController.index();
+      expect(result).toEqual(todoListMock);
+    });
   });
 });
