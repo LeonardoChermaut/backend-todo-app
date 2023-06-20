616bdc205e6929007eb7f6965061cc589f264e73
Leonardo Chermaut <101607060+LeonardoChermaut@users.noreply.github.com>
2023-06-20 20:02:27 -0300

    refactor: added .env port

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,13 @@ import { AppModule } from './domain/views/app';
 import { SwaggerDocumentBuilderModule } from './helpers/swagger/config';
 import { UseGlobalHttpFilter } from './domain/http/exception';
 
+const PORT = process.env.PORT || 3000;
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalFilters(new UseGlobalHttpFilter());
   SwaggerDocumentBuilderModule.setup(app);
-  await app.listen(process.env.PORT);
+
+  await app.listen(PORT);
 }
 bootstrap();
