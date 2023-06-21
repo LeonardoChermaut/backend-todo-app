d53f3cb8101b2156c1030d43cd9ab20fcfa51333
Leonardo Chermaut <101607060+LeonardoChermaut@users.noreply.github.com>
2023-06-21 11:40:28 -0300

    refactor: adjust call mocks

--- src/domain/views/todo/_mock/mock.ts
+++ src/domain/views/todo/_mock/mock.ts
@@ -2,9 +2,9 @@ import { TodoEntity } from '../../../entity';
 import { CreateTodoDto, UpdateTodoDto } from '../dto';
 
 export const listTodoEntityMock: TodoEntity[] = [
-  new TodoEntity({ id: 1, task: 'Todo 1', isDone: false }),
-  new TodoEntity({ id: 2, task: 'Todo 2', isDone: false }),
-  new TodoEntity({ id: 3, task: 'Todo 3', isDone: false }),
+  new TodoEntity({ id: 1, task: 'Todo 1' }),
+  new TodoEntity({ id: 2, task: 'Todo 2' }),
+  new TodoEntity({ id: 3, task: 'Todo 3' }),
   new TodoEntity({ task: 'Todo 99', isDone: true }),
   new TodoEntity({ task: 'Todo 100', isDone: true }),
 ];
@@ -13,6 +13,6 @@ export const todoEntityMock: TodoEntity = { ...listTodoEntityMock[2] };
 
 export const createTodoMock: CreateTodoDto = { ...todoEntityMock };
 
-export const updateTodoMock: UpdateTodoDto = { ...todoEntityMock };
+export const updateTodoMock: UpdateTodoDto = { ...createTodoMock };
 
 export const bodyUpdateTodoMock: UpdateTodoDto = { ...updateTodoMock };
