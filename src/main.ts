10c75a7860b2ec8a56da0a07db78575229d24e07
Leonardo Chermaut <101607060+LeonardoChermaut@users.noreply.github.com>
2023-06-21 17:33:15 -0300

    feat: app main impl global pipe

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,14 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './domain/views/app';
 import { SwaggerDocumentBuilderModule } from './helpers/swagger/config';
 import { UseGlobalHttpFilter } from './domain/http/exception';
+import { UseGlobalValidationPipe } from './domain/validation/common/use.validation.pipe';
 
 const PORT = process.env.PORT || 3000;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalFilters(new UseGlobalHttpFilter());
+  app.useGlobalPipes(new UseGlobalValidationPipe());
   SwaggerDocumentBuilderModule.setup(app);
 
   await app.listen(PORT);
