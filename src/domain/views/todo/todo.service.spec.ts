54622a3bdf096c93303d674b668f199589d2f2d8
Leonardo Chermaut <101607060+LeonardoChermaut@users.noreply.github.com>
2023-06-21 14:30:55 -0300

    refactor:

--- src/domain/views/todo/todo.service.spec.ts
+++ src/domain/views/todo/todo.service.spec.ts
@@ -1,11 +1,13 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { getRepositoryToken } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 import { TodoService } from './todo.service';
 import { TodoEntity } from '../../entity';
 import { listTodoEntityMock, todoEntityMock } from './_mock';
 
 describe('TodoService', () => {
   let todoService: TodoService;
+  let todoRepository: Repository<TodoEntity>;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -16,9 +18,9 @@ describe('TodoService', () => {
           useValue: {
             find: jest.fn().mockResolvedValue(listTodoEntityMock),
             findOneOrFail: jest.fn().mockResolvedValue(todoEntityMock),
-            create: jest.fn(),
+            create: jest.fn().mockReturnValueOnce(todoEntityMock),
+            save: jest.fn().mockResolvedValue(todoEntityMock),
             merge: jest.fn(),
-            save: jest.fn(),
             delete: jest.fn(),
           },
         },
@@ -26,21 +28,25 @@ describe('TodoService', () => {
     }).compile();
 
     todoService = module.get<TodoService>(TodoService);
+    todoRepository = module.get<Repository<TodoEntity>>(
+      getRepositoryToken(TodoEntity),
+    );
   });
 
   it('should be defined', () => {
     expect(todoService).toBeDefined();
+    expect(todoRepository).toBeDefined();
   });
 
   describe('findAll', () => {
     it('should return a list of todo entities successfully', async () => {
       const result = await todoService.findAll();
-
       expect(result).toEqual(listTodoEntityMock);
+      expect(todoRepository.find).toHaveBeenCalledTimes(1);
     });
 
     it('should throw an error if finding todos fails', async () => {
-      jest.spyOn(todoService, 'findAll').mockRejectedValueOnce(new Error());
+      jest.spyOn(todoRepository, 'find').mockRejectedValueOnce(new Error());
       await expect(todoService.findAll()).rejects.toThrowError();
     });
   });
@@ -48,17 +54,31 @@ describe('TodoService', () => {
   describe('findOneOrFail', () => {
     it('should return a todo entity by ID successfully', async () => {
       const result = await todoService.findOneOrFail(3);
-
       expect(result).toEqual(todoEntityMock);
+      expect(todoRepository.findOneOrFail).toHaveBeenCalledTimes(1);
     });
 
     it('should throw an error if todo is not found', async () => {
       jest
-        .spyOn(todoService, 'findOneOrFail')
+        .spyOn(todoRepository, 'findOneOrFail')
         .mockRejectedValueOnce(new Error());
-      await expect(
+      expect(
         todoService.findOneOrFail(todoEntityMock.id),
       ).rejects.toThrowError();
+    });
+  });
+
+  describe('create', () => {
+    it('should create a todo entity successfully', async () => {
+      const result = await todoService.create(todoEntityMock);
+      expect(result).toEqual(todoEntityMock);
+      expect(todoRepository.create).toHaveBeenCalledTimes(1);
+      expect(todoRepository.save).toHaveBeenCalledTimes(1);
+    });
+
+    it('should throw an error if creating todo fails', async () => {
+      jest.spyOn(todoRepository, 'save').mockRejectedValueOnce(new Error());
+      await expect(todoService.create(todoEntityMock)).rejects.toThrowError();
     });
   });
 });
