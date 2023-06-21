dc112cdcd50d63e7308d2ecc9150da592fb810f8
Leonardo Chermaut <101607060+LeonardoChermaut@users.noreply.github.com>
2023-06-21 15:25:48 -0300

    refactor: clear unused todo mock

--- src/domain/views/todo/_mock/mock.ts
+++ src/domain/views/todo/_mock/mock.ts
@@ -5,8 +5,6 @@ export const listTodoEntityMock: TodoEntity[] = [
   new TodoEntity({ id: 1, task: 'Todo 1' }),
   new TodoEntity({ id: 2, task: 'Todo 2' }),
   new TodoEntity({ id: 3, task: 'Todo 3' }),
-  new TodoEntity({ task: 'Todo 99', isDone: true }),
-  new TodoEntity({ task: 'Todo 100', isDone: true }),
 ];
 
 export const todoEntityMock: TodoEntity = { ...listTodoEntityMock[2] };
